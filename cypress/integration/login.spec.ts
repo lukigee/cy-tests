2015845c26348801d0ff8949583f67b14513031d
lukigee <lukeygeisler@gmail.com>
2022-01-10 00:47:54 +0100

    Added first test suite

--- cypress/integration/login.spec.ts
+++ cypress/integration/login.spec.ts
@@ -1,20 +1,32 @@
 /// <reference types="cypress" />
 describe("Login feature", () => {
-  before(() => {
-    cy.visit("/");
-  });
-
-  it("user login to the app", () => {
-    cy.get("a.login").click();
-    cy.get("[name=email]").first().type("test-cy2@yopmail.com");
-    cy.get("[name=passwd]").first().click().type("123445");
-    cy.get("[name=SubmitLogin]").click();
-    cy.contains(".header_user_info", "test test").should("exist");
-    cy.location().should((loc) => {
-      expect(loc.search).to.eq("?controller=my-account");
+  context("Given the user is registered", () => {
+    it("can log in to the app", () => {
+      cy.visit("/index.php");
+      cy.get("a.login").click();
+      cy.get("[name=email]").first().type("test-cy2@yopmail.com");
+      cy.get("[name=passwd]").first().click().type("123445");
+      cy.get("[name=SubmitLogin]").click();
+      cy.contains(".header_user_info", "test test").should("exist");
+      cy.location().should((loc) => {
+        expect(loc.search).to.eq("?controller=my-account");
+      });
     });
   });
   context("Given the user is logged in", () => {
-    it("can log");
+    before(() => {
+      cy.fixture("user.json").as("user");
+      cy.get<{ email: string; password: string }>("@user").then(
+        ({ email, password }) => {
+          cy.login({ email, password });
+        }
+      );
+      cy.visit("/index.php");
+    });
+    it("can log out", () => {
+      cy.get("a.logout").click();
+      cy.contains("#contact-link", "Contact us").should("be.visible");
+      cy.contains("a.login", "Sign in").should("be.visible");
+    });
   });
 });
