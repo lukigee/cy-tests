87720f3b5080f680cf0a799b747629d651464861
lukigee <lukeygeisler@gmail.com>
2022-01-11 00:41:08 +0100

    added new tests

---
+++ cypress/integration/checkout.spec.ts
@@ -0,0 +1,30 @@
+/// <reference types="cypress" />
+
+describe("Checkout feature", () => {
+  context("Given the user is logged in", () => {
+    before(() => {
+      cy.fixture("user.json").then(({ user }) => {
+        cy.login(user);
+      });
+      cy.visit("/index.php");
+      cy.addItemtoCart(5);
+    });
+    it("can complete checkout process using bank wire paymant option", () => {
+      cy.get(".shopping_cart > a").click();
+      cy.contains(".cart_navigation span", "Proceed to checkout").click();
+      cy.contains(".step_current", "Address").then(() => {
+        cy.contains(".cart_navigation span", "Proceed to checkout").click();
+      });
+      cy.contains(".step_current", "Shipping").then(() => {
+        cy.get("#cgv").click();
+        cy.contains(".cart_navigation span", "Proceed to checkout").click();
+      });
+      cy.contains(".step_current", "Payment").then(() => {
+        cy.contains("a.bankwire", "Pay by bank wire").click();
+        cy.contains("button.button", "I confirm my order").click();
+      });
+      cy.contains(".page-heading", "Order confirmation").should("be.visible");
+      cy.contains(".cart_navigation a", "Back to orders").should("be.visible");
+    });
+  });
+});
